acfbd11948574f4c285ee21ab6fd22bde19926a1
wilianx7 <wilianx7@hotmail.com>
2021-03-24 19:49:31 -0300

    Add FragmentManager -- MainActivity

--- app/src/main/java/com/unesc/wslock/activities/MainActivity.java
+++ app/src/main/java/com/unesc/wslock/activities/MainActivity.java
@@ -2,17 +2,21 @@ package com.unesc.wslock.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.unesc.wslock.R;
+import com.unesc.wslock.fragments.LockFragmentList;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,26 +25,38 @@ public class MainActivity extends AppCompatActivity {
 
         this.bottomNavigationView = findViewById(R.id.main_bottom_navigation);
 
+        this.fragmentManager = getSupportFragmentManager();
+
         this.bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.bottom_navigation_home_option:
-                        return true;
-
-                    case R.id.bottom_navigation_locks_option:
-                        return true;
-
-                    case R.id.bottom_navigation_lock_histories_option:
-                        return true;
-
-                    case R.id.bottom_navigation_user_profile_option:
-                        return true;
-
-                    default: return false;
-                }
+                return handleNavigation(item);
             }
         });
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    private boolean handleNavigation(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.bottom_navigation_home_option:
+                return true;
+
+            case R.id.bottom_navigation_locks_option:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.main_container, new LockFragmentList(), null)
+                        .setReorderingAllowed(true)
+//                        .addToBackStack("name") // name can be null
+                        .commit();
+
+                return true;
+
+            case R.id.bottom_navigation_lock_histories_option:
+                return true;
+
+            case R.id.bottom_navigation_user_profile_option:
+                return true;
+
+            default: return false;
+        }
     }
 }
