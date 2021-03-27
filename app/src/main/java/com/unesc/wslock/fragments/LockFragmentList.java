652fbf6d43bed80d006204b8cc966a714eab81d5
wilianx7 <wilianx7@hotmail.com>
2021-03-27 17:49:56 -0300

    Code refactoring to use ListView instead RecyclerView -- LockFragmentList

--- app/src/main/java/com/unesc/wslock/fragments/LockFragmentList.java
+++ app/src/main/java/com/unesc/wslock/fragments/LockFragmentList.java
@@ -1,25 +1,24 @@
 package com.unesc.wslock.fragments;
 
-import android.graphics.drawable.Drawable;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textview.MaterialTextView;
 import com.unesc.wslock.R;
+import com.unesc.wslock.activities.LockFormActivity;
 import com.unesc.wslock.adapters.LockListAdapter;
 import com.unesc.wslock.localstorage.AuthenticatedUser;
+import com.unesc.wslock.models.Lock;
 import com.unesc.wslock.models.lists.LockList;
 import com.unesc.wslock.services.BaseService;
 import com.unesc.wslock.services.LockService;
@@ -29,8 +28,8 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class LockFragmentList extends Fragment {
-    private RecyclerView recyclerView;
-    private LockListAdapter adapter;
+    private ListView lockListView;
+    private LockListAdapter lockListAdapter;
     private ProgressBar progressBar;
     private ImageView illustrationImage;
     private MaterialTextView illustrationDescription;
@@ -41,44 +40,50 @@ public class LockFragmentList extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_lock_list, container, false);
 
-        this.recyclerView = view.findViewById(R.id.lock_recycler_view);
+        this.lockListView = view.findViewById(R.id.fragment_lock_list_view);
         this.progressBar = view.findViewById(R.id.lock_list_progress_bar);
         this.illustrationImage = view.findViewById(R.id.lock_list_illustration);
         this.illustrationDescription = view.findViewById(R.id.lock_list_illustration_description);
         this.floatingActionButton = view.findViewById(R.id.lock_list_floating_action_button);
 
+        this.handleLockListViewItemClick();
         this.handleFloatingButtonClick();
         this.hideIllustrationImage();
-        this.prepareRecyclerView();
         this.loadLocks();
 
         return view;
     }
 
-    private void handleFloatingButtonClick() {
-        this.floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //
-            }
+    private void handleLockListViewItemClick() {
+        this.lockListView.setOnItemClickListener((parent, view, position, id) -> {
+            Lock lock = lockListAdapter.getData().get(position);
+            Intent intent = new Intent(getContext(), LockFormActivity.class);
+
+            intent.putExtra("lock", lock);
+            startActivity(intent);
         });
+    }
+
+    private void handleFloatingButtonClick() {
+        this.floatingActionButton.setOnClickListener(v -> startActivity(new Intent(getContext(), LockFormActivity.class)));
     }
 
     private void loadLocks() {
         this.showProgressBar();
 
         LockService lockService = BaseService.getRetrofitInstance().create(LockService.class);
-        Call<LockList> request = lockService.index("Bearer" + AuthenticatedUser.getToken(getContext()), "createdByUser");
+        Call<LockList> request = lockService.index(AuthenticatedUser.getToken(getContext()), "createdByUser");
 
         request.enqueue(new Callback<LockList>() {
             @Override
             public void onResponse(Call<LockList> call, Response<LockList> response) {
                 if (response.isSuccessful()) {
-                    LockList locks = response.body();
+                    LockList responseBody = response.body();
 
-                    if (response.body().locks.size() > 0) {
-                        adapter = new LockListAdapter(locks);
-                        recyclerView.setAdapter(adapter);
+                    if (responseBody != null && responseBody.locks.size() > 0) {
+                        lockListAdapter = new LockListAdapter(getContext(), responseBody);
+
+                        lockListView.setAdapter(lockListAdapter);
                     } else {
                         showIllustrationImage();
                     }
@@ -94,23 +99,6 @@ public class LockFragmentList extends Fragment {
                 Toast.makeText(getContext(), "Houve um erro ao carregar suas fechaduras", Toast.LENGTH_LONG).show();
             }
         });
-    }
-
-    private void prepareRecyclerView() {
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext());
-        recyclerView.setLayoutManager(layoutManager);
-
-        DividerItemDecoration verticalDecoration = new DividerItemDecoration(this.recyclerView.getContext(), DividerItemDecoration.HORIZONTAL);
-        Drawable verticalDivider = ContextCompat.getDrawable(getActivity(), R.drawable.recycler_view_vertical_divider);
-
-        verticalDecoration.setDrawable(verticalDivider);
-        this.recyclerView.addItemDecoration(verticalDecoration);
-
-        DividerItemDecoration horizontalDecoration = new DividerItemDecoration(this.recyclerView.getContext(), DividerItemDecoration.VERTICAL);
-        Drawable horizontalDivider = ContextCompat.getDrawable(getActivity(), R.drawable.recycler_view_horizontal_divider);
-
-        horizontalDecoration.setDrawable(horizontalDivider);
-        this.recyclerView.addItemDecoration(horizontalDecoration);
     }
 
     private void showProgressBar() {
