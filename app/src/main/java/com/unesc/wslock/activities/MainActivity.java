e6f27a01ff7fdfc939d4557f6a49b1e0dfd7c446
wilianx7 <wilianx7@hotmail.com>
2021-04-01 19:16:09 -0300

    Load HomeFragment & add logout & toolbar menu -- MainActivity

--- app/src/main/java/com/unesc/wslock/activities/MainActivity.java
+++ app/src/main/java/com/unesc/wslock/activities/MainActivity.java
@@ -1,51 +1,75 @@
 package com.unesc.wslock.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.unesc.wslock.R;
-import com.unesc.wslock.fragments.LockFragmentList;
+import com.unesc.wslock.fragments.HomeFragment;
+import com.unesc.wslock.fragments.LockListFragment;
+import com.unesc.wslock.localstorage.AuthenticatedUser;
+import com.unesc.wslock.services.AuthService;
+import com.unesc.wslock.services.BaseService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private BottomNavigationView bottomNavigationView;
     private FragmentManager fragmentManager;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.bottomNavigationView = findViewById(R.id.main_bottom_navigation);
+        this.toolbar = findViewById(R.id.main_activity_toolbar);
+
+        toolbar.setTitle("WS Lock");
+        setSupportActionBar(toolbar);
 
         this.fragmentManager = getSupportFragmentManager();
 
-        this.bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                return handleNavigation(item);
-            }
-        });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.main_bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this::handleNavigation);
+
+        this.fragmentManager.beginTransaction()
+                .replace(R.id.main_container, new HomeFragment(), null)
+                .setReorderingAllowed(true)
+                .commit();
     }
 
     @SuppressLint("NonConstantResourceId")
     private boolean handleNavigation(MenuItem item) {
+        this.toolbar.setVisibility(View.GONE);
+
         switch (item.getItemId()) {
             case R.id.bottom_navigation_home_option:
+                this.toolbar.setVisibility(View.VISIBLE);
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.main_container, new HomeFragment(), null)
+                        .setReorderingAllowed(true)
+                        .commit();
+
                 return true;
 
             case R.id.bottom_navigation_locks_option:
                 fragmentManager.beginTransaction()
-                        .replace(R.id.main_container, new LockFragmentList(), null)
+                        .replace(R.id.main_container, new LockListFragment(), null)
                         .setReorderingAllowed(true)
-//                        .addToBackStack("name") // name can be null
                         .commit();
 
                 return true;
@@ -56,7 +80,48 @@ public class MainActivity extends AppCompatActivity {
             case R.id.bottom_navigation_user_profile_option:
                 return true;
 
-            default: return false;
+            default:
+                return false;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_toolbar_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.main_toolbar_logout_action) {
+            this.logout();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        AuthService authService = BaseService.getRetrofitInstance().create(AuthService.class);
+        Call<Boolean> request = authService.logout(AuthenticatedUser.getToken(MainActivity.this));
+
+        request.enqueue(new Callback<Boolean>() {
+            @Override
+            public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                if (response.isSuccessful()) {
+                    AuthenticatedUser.deleteUserData(MainActivity.this);
+
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Boolean> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Houve um erro ao deslogar", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
