709a593ef364597a66f1da3a005182473ff5218a
wilianx7 <wilianx7@hotmail.com>
2021-04-04 11:36:19 -0300

    Use custom ConfirmationDialog -- LockListAdapter

--- app/src/main/java/com/unesc/wslock/adapters/LockListAdapter.java
+++ app/src/main/java/com/unesc/wslock/adapters/LockListAdapter.java
@@ -1,6 +1,5 @@
 package com.unesc.wslock.adapters;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.unesc.wslock.R;
+import com.unesc.wslock.dialogs.ConfirmationDialog;
 import com.unesc.wslock.fragments.LockListFragment;
 import com.unesc.wslock.localstorage.AuthenticatedUser;
 import com.unesc.wslock.models.Lock;
@@ -29,6 +29,8 @@ public class LockListAdapter extends ArrayAdapter<Lock> {
     private final Context context;
     private final LockListFragment lockListFragment;
     private final List<Lock> data;
+
+    private ConfirmationDialog confirmationDialog;
 
     public LockListAdapter(Context context, LockList lockList, LockListFragment lockListFragment) {
         super(context, 0, lockList.locks);
@@ -75,36 +77,47 @@ public class LockListAdapter extends ArrayAdapter<Lock> {
     }
 
     private void deleteLock(Lock lock) {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this.context, R.style.AlertDialogCustom);
+        View.OnClickListener onConfirm = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                confirmationDialog.dismiss();
 
-        alertDialogBuilder.setTitle("Exclusão de Fechadura");
-        alertDialogBuilder.setMessage("Tem certeza de que deseja excluir? (" + lock.getName() + ")");
-        alertDialogBuilder.setCancelable(true);
+                LockService lockService = BaseService.getRetrofitInstance().create(LockService.class);
+                Call<Boolean> request = lockService.destroy(AuthenticatedUser.getToken(context), String.valueOf(lock.getId()));
 
-        alertDialogBuilder.setPositiveButton("Confirmar", (dialog, which) -> {
-            LockService lockService = BaseService.getRetrofitInstance().create(LockService.class);
-            Call<Boolean> request = lockService.destroy(AuthenticatedUser.getToken(context), String.valueOf(lock.getId()));
+                request.enqueue(new Callback<Boolean>() {
+                    @Override
+                    public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                        if (response.isSuccessful()) {
+                            lockListFragment.loadLocks();
 
-            request.enqueue(new Callback<Boolean>() {
-                @Override
-                public void onResponse(Call<Boolean> call, Response<Boolean> response) {
-                    if (response.isSuccessful()) {
-                        lockListFragment.loadLocks();
-
-                        Toast.makeText(context, "Fechadura excluída com sucesso!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(context, "Fechadura excluída com sucesso!", Toast.LENGTH_SHORT).show();
+                        }
                     }
-                }
 
-                @Override
-                public void onFailure(Call<Boolean> call, Throwable t) {
-                    Toast.makeText(context, "Houve um erro ao excluir a fechadura", Toast.LENGTH_LONG).show();
-                }
-            });
-        });
+                    @Override
+                    public void onFailure(Call<Boolean> call, Throwable t) {
+                        Toast.makeText(context, "Houve um erro ao excluir a fechadura", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        };
 
-        alertDialogBuilder.setNegativeButton("Cancelar", (dialog, which) -> dialog.dismiss());
+        View.OnClickListener onCancel = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                confirmationDialog.dismiss();
+            }
+        };
 
-        alertDialogBuilder.create();
-        alertDialogBuilder.show();
+        this.confirmationDialog = new ConfirmationDialog(
+                this.context,
+                "Exclusão de Fechadura",
+                "Tem certeza de que deseja excluir? (\"" + lock.getName() + "\")",
+                onConfirm,
+                onCancel
+        );
+
+        this.confirmationDialog.show();
     }
 }
