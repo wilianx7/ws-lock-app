19dbccc37323df67877540e65db7515c39c6d1a7
wilianx7 <wilianx7@hotmail.com>
2021-05-11 20:13:47 -0300

    Change base URL to cloud API -- BaseService

--- app/src/main/java/com/unesc/wslock/services/BaseService.java
+++ app/src/main/java/com/unesc/wslock/services/BaseService.java
@@ -4,8 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class BaseService {
-    /** localhost */
-    public static final String BASE_URL = "http://10.0.2.2:8000/";
+    public static final String BASE_URL = "https://api-ws-lock.tecnoelo.com.br/";
 
     private static Retrofit retrofitInstance;
 
